22642bd8ef2ab043cf789704879bde5c12b7d397
Pasindu Tharusha <115170675+MAFiA-6iXTY9iNE@users.noreply.github.com>
2023-03-11 21:05:14 +0530

    Update ChangeUser.sh

--- etc/ChangeUser.sh
+++ etc/ChangeUser.sh
@@ -12,14 +12,7 @@ clear
 #add users
 
 echo -ne "${YELLOW}Enter the username : "; read username
-while true; do
-    read -p "Do you want to genarate a random password ? (Y/N) " yn
-    case $yn in
-        [Yy]* ) password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-9};echo;); break;;
-        [Nn]* ) echo -ne "Enter password (please use a strong password) : "; read password; break;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+echo -ne "Enter password (please use a strong password) : "; read password; break;;
 echo -ne "Enter No. of Days till expiration : ";read nod
 exd=$(date +%F  -d "$nod days")
 chage -E $exd $username && echo "$username:$password" | chpasswd &&
